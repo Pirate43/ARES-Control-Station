f699bdf0a12ce05729751c7f030b39237acde668
Pablo Canseco <p@blocanse.co>
2016-01-27 15:03:07 -0500

    single speed driving implemented

--- ARES_Server/motors.py
+++ ARES_Server/motors.py
@@ -1,27 +1,38 @@
+import serial
 # motor values
 # 125~185 - reverse
 # 186~190 - neutral
 # 191~254 - forward
 
-# LEVEL 1 = 200
-# LEVEL 2 = 210
-# LEVEL 3 = 220
-# LEVEL 4 = 235
-# LEVEL 5 = 254
+LEVEL1 = 200
+LEVEL2 = 210
+LEVEL3 = 220
+LEVEL4 = 235
+LEVEL5 = 254
 
 # Wiring constants (these should be PWM pins on the Arduino)
-PIN_FR = 0
-PIN_FL = 0
-PIN_RR = 0
-PIN_RL = 0
+PIN_FR = 5 #Fronts might be backwards
+PIN_FL = 7
+PIN_RR = 10 #Rears might be backwards
+PIN_RL = 8
 PIN_ACT_R = 0
 PIN_ACT_L = 0
 
+# Open our serial connection
+sp = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=2)
 
 def go_fwd(speed):  # spin all 4 motors forward
     if not speed:
+        sp.write(bytes("a," + str(PIN_FR) + ",200;", 'UTF-8'))
+        sp.write(bytes("a," + str(PIN_FL) + ",200;", 'UTF-8'))
+        sp.write(bytes("a," + str(PIN_RR) + ",200;", 'UTF-8'))
+        sp.write(bytes("a," + str(PIN_RL) + ",200;", 'UTF-8'))
         return "GO FWD - (no speed set)"
     else:
+        sp.write(bytes("a,"+str(PIN_FR)+","+str(level2motor(speed))+";",'UTF-8'))
+        sp.write(bytes("a,"+str(PIN_FL)+","+str(level2motor(speed))+";",'UTF-8'))
+        sp.write(bytes("a,"+str(PIN_RR)+","+str(level2motor(speed))+";",'UTF-8'))
+        sp.write(bytes("a,"+str(PIN_RL)+","+str(level2motor(speed))+";",'UTF-8'))
         return "GO FWD " + str(level2motor(speed))
 
 
@@ -33,6 +44,10 @@ def go_bwd(speed):  # spin all 4 motors backward
 
 
 def halt_motors(): # stop all 4 motors
+    sp.write(bytes("a," + str(PIN_FR) + ",190;", 'UTF-8'))
+    sp.write(bytes("a," + str(PIN_FL) + ",190;", 'UTF-8'))
+    sp.write(bytes("a," + str(PIN_RR) + ",190;", 'UTF-8'))
+    sp.write(bytes("a," + str(PIN_RL) + ",190;", 'UTF-8'))
     return "STOP"
 
 
